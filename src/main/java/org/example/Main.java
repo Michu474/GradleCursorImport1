b22413608cc48ea6aa34e492b8105bd4d5acdd22
Michu474 <magamba474@gmail.com>
2022-10-14 13:06:08 +0300

    gradle-cursor-1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import com.itextpdf.text.pdf.IPdfSpecialColorSpace;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
---
+++ src/test/java/TestGradle.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Resources;
+
+public class TestGradle {
+
+    @Test
+    private void Run()
+    {
+        System.out.println("Nothing");
+
+    }
+}
